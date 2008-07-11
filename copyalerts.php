21dcafd6ca26b4c4c53c73ec07fbb67f915e6260
markpdxt <markpdxt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-11 20:08:06 +0000

    added new sample images
    git-svn-id: https://plugins.svn.wordpress.org/copy-alerts/trunk@54324 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- copyalerts.php
+++ copyalerts.php
@@ -27,8 +27,8 @@ Author URI: http://blog.bitscan.com/copyalerts-wordpress-plugin/
 
 function wpca_show_form()
 {
-	echo '<p>Content monitored by <a href="http://www.copyalerts.com/">';
-	echo 'Copy Alerts</a></p>' . "\n";
+	echo '<p><a href="http://www.copyalerts.com/">';
+	echo 'Copy Alerts</a> Monitored</p>' . "\n";
 }
 
 function wpca_install()
